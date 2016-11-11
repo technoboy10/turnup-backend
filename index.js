3b1390725360081a2bd30711c78229eb3fa9f5e4
Connor Hudson <technoboy10@users.noreply.github.com>
2016-11-10 18:25:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require('./lib/util/server');
 
-console.log(process.env); //eslint-disable-line
 server.listen((process.env.PORT || 8080), function () {
     console.log('%s listening at %s', server.name, server.url); //eslint-disable-line
 });
